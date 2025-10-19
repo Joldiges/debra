af30f41015193f7e37fb15087b691a93bac4295b
james oldiges <jamesoldiges@gmail.com>
2025-10-19 12:47:22 -0400

    .

---
+++ scripts/backups/restore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# rsync a backup but use excludes from excludes.txt which is in the same folder
+
+EXCLUDES_FILE="$(dirname "$0")/excludes.txt"
+SOURCE="/"
+DESTINATION="/mnt/backup/"
+RSYNC_OPTIONS="-aAXv --delete"
+RSYNC_EXCLUDES=""
+while IFS= read -r line; do
+    RSYNC_EXCLUDES+="--exclude=\"$line\" "
+done < "$EXCLUDES_FILE"
+RSYNC_COMMAND="rsync $RSYNC_OPTIONS $RSYNC_EXCLUDES \"$DESTINATION\" \"$SOURCE\""
+eval "$RSYNC_COMMAND"
+echo "Backup completed successfully."
