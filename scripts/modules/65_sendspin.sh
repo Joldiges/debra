b2f1b4462d035dc8ebf20d4bb4542d0626d43c4e
james oldiges <jamesoldiges@gmail.com>
2026-01-10 04:05:46 -0500

    Raspi UV

--- scripts/modules/65_sendspin.sh
+++ scripts/modules/65_sendspin.sh
@@ -10,6 +10,10 @@ if [[ ${ENABLE_SENDSPIN:-0} != "1" ]]; then
 fi
 
 wget https://raw.githubusercontent.com/Sendspin/sendspin-cli/main/scripts/systemd/install-systemd.sh -O /tmp/install-sendspin.sh
+# Have SendSpin use piwheels and local builds.  Neither will break anything if not on a Raspberry Pi.
+sudo sed -i \
+'s|bash -l -c "uv tool install sendspin"|bash -l -c "uv tool install sendspin --index-url https://pypi.org/simple --extra-index-url https://www.piwheels.org/simple --find-links file:///home/'"$USER"'/git/debra/legacy/raspi0/wheels"|' \
+/tmp/install-sendspin.sh
 sudo bash /tmp/install-sendspin.sh
 rm /tmp/install-sendspin.sh
 
